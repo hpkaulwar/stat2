ba391c281c0b7357f9db64524f01b27edaa0ffe4
panchbhai1969 <panchbhai1969@gmail.com>
2019-04-23 17:20:19 +0530

    Fixed multiple file changes issue

--- script.sh
+++ script.sh
@@ -29,25 +29,35 @@ for i in "${!commit[@]}"; do
     then
       continue  
     fi
-    commit_changes=$(git log --stat --max-count=1 ${commit[i]} | grep "|" | awk '{print $3}')
-    stuff=($commit_changes) 
-    #echo $commit_changes
-    len=${#stuff[@]} 
-    #echo $len
-    zero=0
-    
-    if [ $len -eq $zero ]
+    re='^[0-9]+$'
+    temp=0
+    commit_changes=($(git log --stat --max-count=1 ${commit[i]} | grep "|" | awk '{print $3}'))
+    for v in "${!commit_changes[@]}"; do
+        if ! [[ ${commit_changes[v]} =~ $re ]] ; then
+            #echo ${commit_changes[i]}
+            #echo "error: Not a number" >&2;
+            continue
+        fi
+        temp=$(($temp + ${commit_changes[v]}))
+    done
+    #echo $temp
+    commit_changes=$temp
+    zero=0  
+    if [ $commit_changes -eq $zero ]
     then
       continue  
     fi
 
     commit_final+=(${commit[i]})
-    change_final+=($commit_changes)     
+    change_final+=($commit_changes) 
+    #echo -----------------
+    #echo ${commit[i]}    
+    #echo $commit_changes
 done
 
 for i in "${!commit_final[@]}"; do
-    #echo ${commit_final[i]}
-    #echo ${change_final[i]}
+    echo ${commit_final[i]}
+    echo ${change_final[i]}
     continue
 
 done
@@ -55,7 +65,7 @@ done
 
 for i in "${!change_final[@]}"; do
     #printf '${arr[%s]}=%s\n' "$i" "${arr[i]}"
-    if [ ${arr[i]} -lt $val ]
+    if [ ${change_final[i]} -lt $val ]
         then
             index+=($i)
             #echo ${arr[i]}
@@ -66,8 +76,12 @@ done
 for i in "${!index[@]}"; do
     #printf '${arr[%s]}=%s\n' "$i" "${arr[i]}"
     ind=${index[i]} 
+    #echo Iter: $i
+    #echo Index: $ind
+    #echo ${!commit_final[@]}
+    #echo ${!change_final[@]}
     echo commit hash_val :${commit_final[ind]}
-    echo Lines changed  ${change_final[ind]} 
+    echo Lines changed  :${change_final[ind]} 
     echo "**************"
     
 done
